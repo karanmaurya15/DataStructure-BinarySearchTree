b4b19d03486f73e06b430521968c03e6addf06a2
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-08 10:26:10 +0530

    Binary Search Tree is created

--- DataStructure-BinarySearchTree/Program.cs
+++ DataStructure-BinarySearchTree/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Binary Search Tree");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
         }
     }
 }
